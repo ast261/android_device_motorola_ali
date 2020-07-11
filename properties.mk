dbbf794ce493102f1e35040d271d74c3c0a6df97
jeangraff30 <jeangraff30@gmail.com>
2020-07-11 17:42:37 -0300

    ali: Update audio from LA.UM.8.6.r1-04400-89xx.0
    Update ACDB from LA.UM.8.6.r1-02600-89xx.0

--- properties.mk
+++ properties.mk
@@ -44,19 +44,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.voice.path.for.pcm.voip=true \
     vendor.audio.offload.min.duration.secs=60
 
+# Vol
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.vc_call_vol_steps=7 \
 	ro.config.media_vol_steps=25
-
-# Audio ACDB
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/common/Bluetooth_cal.acdb \
-    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/common/General_cal.acdb \
-    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/common/Global_cal.acdb \
-    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/common/Handset_cal.acdb \
-    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/common/Hdmi_cal.acdb \
-    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/common/Headset_cal.acdb \
-    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/common/Speaker_cal.acdb
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
